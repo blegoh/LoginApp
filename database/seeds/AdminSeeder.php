5fa33d3ae163421214e336a53d1087efb559fc2d
blegoh <yusufblegoh@gmail.com>
2017-04-30 07:30:14 +0700

    add new migration and seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        \App\Admin::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@app.com',
+                'password' => bcrypt('admin')
+            ]
+        );
+    }
+}
